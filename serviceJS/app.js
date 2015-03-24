ad6f5de6344d2b7260c7b398235928d15e8298c7
Estefano Castañeda <mixc21@gmail.com>
2015-03-24 17:42:32 -0500

    adding token

--- serviceJS/app.js
+++ serviceJS/app.js
@@ -1,14 +1,18 @@
 var express = require('express')
 var app = express()
 var mongodb = require('mongolab-provider').init('pinbuydb', 'o5wMMdzdsFiwqsD6Pd-gh2-rCRmUnk4N');
+var jwt = require('jsonwebtoken');
 
+app.set('json spaces', 2);
 
 app.get('/api/user/:user/:key', function(req, res) {
   
-	mongodb.documents('user', {}, function(err,data){
+	mongodb.documents('user', {where:{username:req.params.user,password:req.params.key}}, function(err,data){
 
 
 		var auth={};
+
+		var token = jwt.sign(data[0], 'shhhhh');
 
 		if(req.params.user===data[0].username){
 			auth.nameExist=true;
@@ -18,6 +22,7 @@ app.get('/api/user/:user/:key', function(req, res) {
 
 		if(req.params.key===data[0].password){
 			auth.passValid=true;
+			auth.token = token;
 		}else{
 			auth.passValid=false;
 		}	
